2655ad6e7bef6a0c043786ac3ea53eec5d2a5a66
Eduardo <eduardoschiavo09@gmail.com>
2022-04-21 14:09:58 +0200

    comments updated

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from  flask import Flask, redirect, url_for
-
 from flask import render_template, request
 
+#instantiate flask app
 app =Flask('__main__')
 
+#route to the homepage, using the index.html template
 @app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("index.html")
 
+#route to the form that lets you enter your name. 
+#not a real login by any means!!
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
@@ -15,12 +18,13 @@ def login():
         return redirect(url_for("user", username=username))
     else:
         return render_template("login.html")
-    
+
+#route displaying welcome message
 @app.route("/<username>")
 def user(username):
-    # return "<h1>{0}</h1>".format(username)
     return render_template("username.html", name=username)
 
 
+#run the app
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True) #debug True let's the app automatically updated if changes are saved while it's running
